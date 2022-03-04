d107b24d6e1c96a53086e3d48ac9e37113243d57
rayziz <rayziz@mail.ru>
2022-03-04 20:50:51 +0300

    add Homework1.3

---
+++ Homework1.3_chetn/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Добрый день. Я универсальный помощник по определению чётности чисел");
+Console.WriteLine("От Вас требуется только указать число, а всё остальное я беру на себя)");
+string strValue = Console.ReadLine();
+int value = int.Parse(strValue);
+
+if((value % 2) == 0) Console.WriteLine("Это самое чётное число в мире");
+else Console.WriteLine("Не видел ничего более нечётнёго в мире");
