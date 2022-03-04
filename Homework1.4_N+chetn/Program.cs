f205512ead28db31e48c78014d2e1320417bd227
rayziz <rayziz@mail.ru>
2022-03-04 21:22:33 +0300

    add Homework1.4

---
+++ Homework1.4_N+chetn/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Добрый день. Я ещё более универсальный помощник по определению чётности чисел");
+Console.WriteLine("От Вас требуется только указать число, а всё остальное я беру на себя)");
+string strValue = Console.ReadLine();
+int value = int.Parse(strValue);
+int index = 2;
+int array = 0;
+while (index <= value)
+{
+    array = index;
+    Console.Write($"{array}, ");
+    index = index+2;
+}
+Console.WriteLine();
