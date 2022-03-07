774b059b6e326ab6138138f69577c702583075ed
rayziz <rayziz@mail.ru>
2022-03-07 20:25:11 +0300

    add Example013

---
+++ Example013_3znak/Program.cs
@@ -0,0 +1,15 @@
+﻿//int value1 = 456;
+//int value2 = 782;
+//int value3 = 918;
+
+//Var1
+int[] array1 = { 9, 1, 8};
+int value1 = array1[2];
+Console.WriteLine(value1);
+
+
+//Var2
+string A = Console.ReadLine();
+int B = int.Parse(A);
+int last = B % 10;
+Console.WriteLine(last);
