902048b4a3dc8cea6fdc5ade13606033e663893c
rayziz <rayziz@mail.ru>
2022-03-07 22:13:52 +0300

    add Example014

---
+++ Example014_maxIzRnd/Program.cs
@@ -0,0 +1,17 @@
+﻿Random rnd = new Random();
+int value = rnd.Next(10, 99);
+Console.WriteLine(value);
+
+
+if (value/10 > value%10)
+{
+    Console.WriteLine(value/10);
+}
+if (value/10 < value%10)
+{
+    Console.WriteLine(value%10);
+}
+if (value/10 == value%10)
+{
+    Console.WriteLine($"ОНИ РАВНЫ {value/10}");
+}
