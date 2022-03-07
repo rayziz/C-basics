5f3f715fb51896e739f669ddcd50f7d265aa627d
rayziz <rayziz@mail.ru>
2022-03-07 22:20:02 +0300

    add Homework2.1

---
+++ Homework2.1_davIzTreh/Program.cs
@@ -0,0 +1,11 @@
+﻿//456
+//782
+//918
+
+Console.WriteLine("Введите трёхзначное число");
+string A = Console.ReadLine();
+int B = int.Parse(A);
+int secondPlus = B % 100;
+int second = secondPlus / 10;
+Console.WriteLine(second);
+
