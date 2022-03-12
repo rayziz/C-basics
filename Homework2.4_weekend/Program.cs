3fe3849657869dfe8bcb68f68eda211584d25045
rayziz <rayziz@mail.ru>
2022-03-12 21:04:22 +0300

    add Homework2.4

---
+++ Homework2.4_weekend/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Сейчас посмотрим надо ли сегодня работать. Вводи порядковый день недели");
+string strValue = Console.ReadLine();
+int value = int.Parse(strValue);
+
+if (value == 1 | value == 2 | value == 3 | value == 4 | value == 5) Console.WriteLine("Соболезную и удачи на работе");
+if (value == 6 || value == 7) Console.WriteLine("Поздравляю, Пина колада на столе");
+if (value > 7) Console.WriteLine("В неделе семь дней, ещё раз так ошибёшься и на работу пойдёшь");
