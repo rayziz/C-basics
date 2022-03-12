6bf30ecb4a5f955e97e09372735a991150601002
rayziz <rayziz@mail.ru>
2022-03-12 22:15:34 +0300

    add Homework2.3

---
+++ Homework2.3_thiSym/Program.cs
@@ -0,0 +1,15 @@
+﻿int result = 0;
+Console.WriteLine("Введите число");
+string strValue = Console.ReadLine();
+int value = int.Parse(strValue);
+while (value > 999)
+{
+    value = value / 10;
+}
+
+if (value > 99) 
+{
+    result = value % 10;
+    Console.WriteLine($"Третья цифра {result}");
+}
+else Console.WriteLine("Третьей цифhы нет");
