c41b4bf583da30feea82087523fa7ba06b4371d3
rayziz <rayziz@mail.ru>
2022-03-12 20:12:09 +0300

    add Homework2.2

---
+++ Homework2.2_3rnd-2/Program.cs
@@ -0,0 +1,11 @@
+﻿Random rnd = new Random();
+int value = rnd.Next(100, 1000);
+Console.WriteLine(value);
+
+Console.WriteLine("Нажмите любую клавишу, чтобы была магия");
+Console.ReadLine();
+
+int firSymbol = (value/100);
+int secSymbol = (value%10);
+
+Console.WriteLine($"{firSymbol}{secSymbol}");
