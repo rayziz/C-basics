f8e9097d3aa32860f57f31f9b93477757d4b97b2
rayziz <rayziz@mail.ru>
2022-03-13 18:10:13 +0300

    add Example017

---
+++ Example017_koordinat/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Сектор четверть на барабане. Ой да ладно я просто шучу, назовите четверть");
+string strValue = Console.ReadLine();
+int value = int.Parse(strValue);
+if (value == 1)
+{
+    Console.WriteLine("X > 0 и Y > 0");
+} 
+else if (value == 2)
+{
+    Console.WriteLine("X < 0 и Y > 0");
+} 
+else if (value == 3)
+{
+    Console.WriteLine("X < 0 и Y < 0");
+} 
+else if (value == 4)
+{
+    Console.WriteLine("X > 0 и Y < 0");
+} 
+else Console.WriteLine("Сударь, в школе до какого класса учились? От 1 до 4 введите.");
