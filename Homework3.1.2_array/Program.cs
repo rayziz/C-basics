500e3296af4a11a35ed50cb127fadde9cd458690
rayziz <rayziz@mail.ru>
2022-03-18 17:12:49 +0300

    add homework 3 is done

---
+++ Homework3.1.2_array/Program.cs
@@ -0,0 +1,25 @@
+﻿int Prompt(string message)
+{
+Console.WriteLine(message);
+string strValue = Console.ReadLine();
+int Value = int.Parse(strValue);
+return Value;
+}
+
+int val = Prompt("Введите в строчку 5 цифр");
+
+int firSymbol = (val/10000);
+int secSymbol = (val/1000%10);
+int fouSymbol = (val%100/10);
+int fifSymbol = (val%10);
+
+if (firSymbol == fifSymbol && secSymbol == fouSymbol)
+{   
+     Console.WriteLine("Ну что палиндром, попался");
+}
+else 
+{
+     Console.WriteLine("Товарищи, мы его упустили");
+}
+
+
