52ae35755bc5c294969f1db3145fc73096675c3f
rayziz <rayziz@mail.ru>
2022-03-18 16:56:43 +0300

    add Homework3.1.1_+

--- Homework3.1.1_string/Program.cs
+++ Homework3.1.1_string/Program.cs
@@ -2,8 +2,10 @@
 string strValue = Console.ReadLine();
 
 if (strValue[0] == strValue[4] && strValue[1] == strValue[3])
-{
-    Console.WriteLine("Ну что палиндром, попался");
+{   
+     Console.WriteLine("Ну что палиндром, попался");
 }
-else Console.WriteLine("Товарищи, мы его упустили");
-
+else 
+{
+     Console.WriteLine("Товарищи, мы его упустили");
+}
