a9e6e78a44c54f778848eb4a9fab5261d8bf4be3
rayziz <rayziz@mail.ru>
2022-03-18 16:14:04 +0300

    add Homework3.1.1

---
+++ Homework3.1.1_string/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите в строчку 5 цифр");
+string strValue = Console.ReadLine();
+
+if (strValue[0] == strValue[4] && strValue[1] == strValue[3])
+{
+    Console.WriteLine("Ну что палиндром, попался");
+}
+else Console.WriteLine("Товарищи, мы его упустили");
+
