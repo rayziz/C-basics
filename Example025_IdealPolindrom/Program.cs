014b06b7aa97cdec1808047d83de9869f66a27a1
rayziz <rayziz@mail.ru>
2022-03-25 16:45:22 +0300

    add Example025

---
+++ Example025_IdealPolindrom/Program.cs
@@ -0,0 +1,32 @@
+﻿// Идеальный полиндром для букв и цифр
+
+Console.WriteLine("Введите что-то, чтобы я мог повыпенидриваться");
+string strValue = Console.ReadLine();
+
+if (IsPalindrom(strValue))
+{
+    Console.WriteLine("Это Палиндром лови его");
+}
+else
+{
+    Console.WriteLine("Мы его потеряли");
+}
+
+
+// Далее функция IsPalindrom, которая как раз проверяет палиндром ли перед нами или нет
+
+bool IsPalindrom(string digital)
+{
+    int len = digital.Length;
+    int i = 0;
+    while (i < len / 2)
+    {
+        if (digital[i] != digital[len - i - 1])
+        {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+
