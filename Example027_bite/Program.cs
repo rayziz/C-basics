64d15d33a673d5af49faf1828e48f0226c7ee675
rayziz <rayziz@mail.ru>
2022-03-25 21:30:36 +0300

    add Example026

---
+++ Example027_bite/Program.cs
@@ -0,0 +1,11 @@
+﻿int i = 0;
+Console.WriteLine("Введите число");
+string strValue = Console.ReadLine();
+int value = int.Parse(strValue);
+while (value > 0)
+{
+    value = value / 10;
+    i++;
+}
+
+ Console.WriteLine($"В числе {i} знаков");
