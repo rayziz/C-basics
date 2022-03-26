9e8f6890ff66722ea75d4d2856ab5005d91352a7
rayziz <rayziz@mail.ru>
2022-03-26 13:14:07 +0300

    add Example028

---
+++ Example028_umnozhDoN/Program.cs
@@ -0,0 +1,19 @@
+﻿// программа принимает на вход число и считает произведение от 1 до N
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int ProizvedenieDoN(digit)
+{
+    int proiz = 1;
+    for (int i = 0; i < digit; i++)
+    {
+        int proiz = proiz*digit;
+    }
+    return proiz;
+}
