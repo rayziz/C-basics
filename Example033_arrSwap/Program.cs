f9a9b07fca8386992f25978e4e4b80e03ff13a1b
rayziz <rayziz@mail.ru>
2022-04-02 20:31:52 +0300

    add Example033

---
+++ Example033_arrSwap/Program.cs
@@ -0,0 +1,47 @@
+﻿/**** Задача 1: Напишите программу, которая перевернёт одномерный массив
+*(последний элемент будет на первом месте, а первый - на последнем и т.д.)
+*/
+int[] RandArray(int lenght)
+{
+int[] answer = new int[lenght];
+for (int i = 0; i < lenght; i++)
+{
+answer[i] = new Random().Next(1, 5);
+}
+return answer;
+}
+void PrintArr(int[] Mas)
+{
+int count = Mas.Length;
+for (int pos = 0; pos < count - 1; pos++)
+{
+Console.Write(Mas[pos] + " ,");
+}
+Console.WriteLine(Mas[Mas.Length - 1]);
+}
+
+void Reverse(int[] arr)
+{
+for (int i = 0; i < arr.Length / 2; i++)
+{
+Swap2(arr, i, arr.Length - 1 - i);
+}
+}
+
+void Swap(int[] arr, int i, int j)
+{
+(arr[i], arr[j]) = (arr[j], arr[i]);
+}
+
+void Swap2(int[] arr, int i, int j)
+{
+int tmp = arr[i];
+arr[i] = arr[j];
+arr[j] = tmp;
+}
+
+
+var array = RandArray(5);
+PrintArr(array);
+Reverse(array);
+PrintArr(array);
