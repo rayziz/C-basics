72f98f686ff2ff60d09c0f917fdcbb3a792af4a7
rayziz <rayziz@mail.ru>
2022-04-02 16:24:31 +0300

    add Homework4.2

---
+++ Homework4.2_sum/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int.TryParse(Console.ReadLine(), out int value);
+    return value;
+}
+
+int Summa(int val)
+{
+    int sum = 0;
+    while (val != 0)
+    {
+        sum = sum + val % 10;
+        val = val / 10;
+    }
+    return sum;
+}
+
+int val = Prompt("Введите число");
+
+Console.WriteLine(Summa(val));
+
+
+
+
+
