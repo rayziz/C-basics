756b77ec8f712f5a9aeefd510088ef25ce1b702c
rayziz <rayziz@mail.ru>
2022-04-02 16:59:20 +0300

    add Homework4.3

---
+++ Homework4.3_RanArray/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов случайными числами и выводит их на экран. 
+//Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах)
+
+int[] RandArray(int lenght)
+{
+    int[] answer = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        answer[i] = new Random().Next(1, 100);
+    }
+    return answer;
+}
+
+void PrintArray(int[] Mas)
+{
+    int count = Mas.Length;
+    for (int pos = 0; pos < count; pos++)
+    {
+        Console.Write(Mas[pos] + ", ");
+    }
+    Console.WriteLine();
+}
+
+int[] arr = RandArray(8);
+PrintArray(arr);
+
