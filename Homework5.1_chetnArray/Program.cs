c536d701b5c0b1eb9dbb47fdd80c18b7f1a9fcdb
rayziz <rayziz@mail.ru>
2022-04-02 17:55:15 +0300

    add Homework5.1

---
+++ Homework5.1_chetnArray/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+int chetn = 0;
+int[] arr = RandArray(5);
+PrintArray(arr);
+
+int len = arr.Length;
+for (int i = 0; i < len; i++)
+{
+    if (arr[i] % 2 == 0)
+    {
+        chetn++;
+    }
+    
+}
+
+Console.WriteLine(chetn);
+
+int[] RandArray(int lenght)
+{
+    int[] answer = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        answer[i] = new Random().Next(100, 999);
+    }
+    return answer;
+}
+
+void PrintArray(int[] Mas)
+{
+    int count = Mas.Length;
+    for (int pos = 0; pos < count; pos++)
+    {
+        Console.Write(Mas[pos] + ", ");
+    }
+    Console.WriteLine();
+}
+
+
