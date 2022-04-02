232f498dfb051bd0bc9b56c234a1e5846c653758
rayziz <rayziz@mail.ru>
2022-04-02 12:25:32 +0300

    add Homework4.1

--- Example003/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿int numberA = new Random().Next(1, 10);
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1, 10);
-Console.WriteLine(numberB);
-Console.WriteLine(numberA + numberB);
---
+++ Homework4.1_stepen/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int i = 1;
+
+int ValA = Prompt("Введите A");
+int ValB = Prompt("Введите B");
+
+int ValATemp = ValA;
+
+while (ValB > i)
+{
+    ValA = ValA * ValATemp;
+    i++;
+}
+
+Console.WriteLine($"{ValA}");
