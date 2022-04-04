ae00cd4f1ab86fbb5078c826a14589f0bf3ed450
rayziz <rayziz@mail.ru>
2022-04-04 13:12:02 +0300

    add Example033-036

---
+++ Example035_10to2/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 2: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+//45 -> 101101
+//3  -> 11
+//2  -> 10
+
+int val = Prompt("Введите число");
+int [] dvoichArray = new int [10];
+int i = 0;
+int ostatok = 0;
+
+while(val >= 1)
+{
+    ostatok = val % 2;
+    val = val / 2;
+    dvoichArray[i] = ostatok;
+    i++;
+    Console.Write(ostatok);
+} 
+Console.WriteLine();
+
+for (int j = i-1; j >=0; j--)
+Console.Write(dvoichArray[j]);
+
+Console.WriteLine();
+
+// Methods
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
---
+++ Example036_Fibonach/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача 3: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+//Если N = 5 -> 0 1 1 2 3
+//Если N = 3 -> 0 1 1
+//Если N = 7 -> 0 1 1 2 3 5 8
+
+int count = Prompt("Введите число Фибоначи");
+
+int [] fibonachArray = new int [count];
+
+int n1 = 0;
+int n2 = 1;
+
+fibonachArray[0] = n1;
+fibonachArray[1] = n2;
+
+for (int i = 2; i < count; i++)
+{
+    int n3 = n1 + n2;
+    fibonachArray[i] = n3;
+    n1 = n2;
+    n2 = n3;
+}
+
+foreach (int element in fibonachArray)
+{
+    Console.WriteLine(element);
+}
+
+
+// Methods
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
