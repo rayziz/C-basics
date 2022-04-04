f06c53cf9229a768446bb247f4c89d263817a50d
rayziz <rayziz@mail.ru>
2022-04-04 13:13:36 +0300

    add Zabitiyi

---
+++ Example030_newZadaxha/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 1: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] RandArray(int lenght)
+{
+    int[] answer = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        answer[i] = new Random().Next(-9, 10);
+    }
+    return answer;
+}
+void printArr(int[] Mas)
+{
+    int count = Mas.Length;
+    for (int pos = 0; pos < count; pos++)
+    {
+        Console.Write(Mas[pos] + " ,");
+    }
+    Console.WriteLine();
+}
+
+int[] arr = RandArray(4);
+printArr(arr);
+
+for(int i = 0; i < 4; i++)
+{
+    arr[i] = arr[i]*(-1);
+}
+printArr(arr);
---
+++ Example032_Pari/Program.cs
@@ -0,0 +1,47 @@
+﻿//Задача 4: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+//[1 2 3 4 5] -> 5 8 3
+//[6 7 3 6] -> 36 21
+
+
+int[] RandArray(int lenght)
+{
+    int[] answer = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        answer[i] = new Random().Next(-100, 100);
+    }
+    return answer;
+}
+void printArr(int[] Mas)
+{
+    int count = Mas.Length;
+    for (int pos = 0; pos < count; pos++)
+    {
+        Console.Write(Mas[pos] + " ,");
+    }
+    Console.WriteLine();
+}
+
+
+int[] arr = RandArray(12);
+printArr(arr);
+
+//int len = arr.Length(); 
+
+void Exist(int[] arr)
+{
+
+
+    for (int i = 0; i < 12/2; i++)
+    {
+        
+       // proizv = arr[i] * arr[len - i - 1];
+        Console.WriteLine($"{arr[i] * arr[12 - i - 1]} ");
+            
+    }
+
+}
+
+Exist(arr);
+
+
