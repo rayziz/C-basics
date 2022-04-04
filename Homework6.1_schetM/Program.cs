afee0cfac237820adab98653a08cd7712f55db3b
rayziz <rayziz@mail.ru>
2022-04-04 19:57:10 +0300

    add Homework6.1

---
+++ Homework6.1_schetM/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//-1, -7, 567, 89, 223-> 3
+
+
+int count = Prompt("Количество числе на проверку?");
+int j = 0;
+
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine("Ведите число");
+    int chekedCount = int.Parse(Console.ReadLine());
+    if (chekedCount > 0)
+    {
+        j++;
+    }
+}
+
+Console.WriteLine(j);
+
+// Methods
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
