de2039af59f47dd733943b03cfea0139374284ae
rayziz <rayziz@mail.ru>
2022-04-04 15:26:35 +0300

    add Homework6.2

---
+++ Homework6.2_neravenstvo/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double b1 = Prompt("Введите число b1");
+double b2 = Prompt("Введите число b2");
+double k1 = Prompt("Введите число k1");
+double k2 = Prompt("Введите число k2");
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Координата x = {x}, координата y = {y}");
+
+
+// Methods
+double Prompt(string message)
+{
+    Console.WriteLine(message);
+    string strValue = Console.ReadLine();
+    double Value = double.Parse(strValue);
+    return Value;
+}
