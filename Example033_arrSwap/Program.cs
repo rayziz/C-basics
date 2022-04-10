c0230a361dcb4091ee016663cf7518a5c60c1e06
rayziz <rayziz@mail.ru>
2022-04-10 16:08:00 +0300

    add Example041

--- Example033_arrSwap/Program.cs
+++ Example033_arrSwap/Program.cs
@@ -3,41 +3,41 @@
 */
 int[] RandArray(int lenght)
 {
-int[] answer = new int[lenght];
-for (int i = 0; i < lenght; i++)
-{
-answer[i] = new Random().Next(1, 5);
-}
-return answer;
+    int[] answer = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        answer[i] = new Random().Next(1, 5);
+    }
+    return answer;
 }
 void PrintArr(int[] Mas)
 {
-int count = Mas.Length;
-for (int pos = 0; pos < count - 1; pos++)
-{
-Console.Write(Mas[pos] + " ,");
-}
-Console.WriteLine(Mas[Mas.Length - 1]);
+    int count = Mas.Length;
+    for (int pos = 0; pos < count - 1; pos++)
+    {
+        Console.Write(Mas[pos] + " ,");
+    }
+    Console.WriteLine(Mas[Mas.Length - 1]);
 }
 
 void Reverse(int[] arr)
 {
-for (int i = 0; i < arr.Length / 2; i++)
-{
-Swap2(arr, i, arr.Length - 1 - i);
-}
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        Swap2(arr, i, arr.Length - 1 - i);
+    }
 }
 
 void Swap(int[] arr, int i, int j)
 {
-(arr[i], arr[j]) = (arr[j], arr[i]);
+    (arr[i], arr[j]) = (arr[j], arr[i]);
 }
 
 void Swap2(int[] arr, int i, int j)
 {
-int tmp = arr[i];
-arr[i] = arr[j];
-arr[j] = tmp;
+    int tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
 }
 
 
---
+++ Example041_change/Program.cs
@@ -0,0 +1,55 @@
+﻿//Задача 1: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+
+int[,] array = FillArray(5, 4);
+PrintArray(array);
+Reverse(array);
+Console.WriteLine();
+PrintArray(array);
+
+
+// Methods
+int[,] FillArray(int row, int column)
+{
+    int[,] array = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+void Reverse(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        int[] Temp1 = new int[array.GetLength(0)];
+        int[] Temp2 = new int[array.GetLength(0)];
+
+        Temp1[j] = array[0, j];
+        Temp2[j] = array[4, j];
+
+        array[0, j] = Temp2[j];
+        array[4, j] = Temp1[j];
+    }
+}
+
+
