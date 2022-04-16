9d20da1000f14cb64cf3ebfb5480176d39611afc
rayziz <rayziz@mail.ru>
2022-04-16 16:14:48 +0300

    add Example044

---
+++ Example044_Rec+1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 1: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N при помощи рекурсии
+
+int N = Prompt("Введите N");
+
+Plus1(N);
+
+
+// Methods
+
+void Plus1(int N)
+{
+    if (N == 0) return;
+    Plus1(N-1);
+    Console.Write(N + ", ");
+
+}
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
