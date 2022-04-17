5aca8c269c4604b35f3e3aeab45f10a9d1f7f9a7
rayziz <rayziz@mail.ru>
2022-04-17 17:30:05 +0300

    add Example050

---
+++ Example049_RecPredSum/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+//3 и 4, N = 5 -> 3 4 7 11 18
+//6 и 10, N = 4 -> 6 10 16 26 
+
+
+Console.WriteLine(PlusPredTwo(3, 4, 5));
+
+
+// Methods
+
+int PlusPredTwo(int first, int second, int N)
+{
+    if (N == 0) return 0;
+    Console.Write($"{first}, ");
+    int temp = PlusPredTwo(second, first + second, N - 1) + first;
+    return temp;
+
+}
---
+++ Example050_Alphabet/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 2: В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита.
+//n = 2 -> аа, ии, сс, вв, аи, иа, ис, си, ас, са, 
+//ав, ва, ви, ив, св, вс
+int n = 1;
+Find("аисв", new char[2]);
+
+
+// Methods
+
+void Find(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($" {n++} {new String (word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        Find(alphabet, word, length + 1);
+    }
+}
+
+// Methods
