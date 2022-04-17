c9d26aa18f7137de00a00f1c9b525da7e5bee412
rayziz <rayziz@mail.ru>
2022-04-17 11:40:49 +0300

    add Example047

---
+++ Example047_RecChetn/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 5 -> "2, 4"
+//M = 4; N = 8 -> "4, 6, 8"
+
+
+int M = Prompt("Введите M");
+int N = Prompt("Введите N");
+
+PlusChetn(M, N);
+
+
+// Methods
+
+void PlusChetn(int M, int N)
+{
+    if (M > N) return;
+    if (M % 2 == 1)
+    {
+        M++;
+        Console.Write(M + ", ");
+        PlusChetn(M + 2, N);
+    }
+    else if (M % 2 == 0)
+    {
+        Console.Write(M + ", ");
+        PlusChetn(M + 2, N);
+    }
+}
+
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
