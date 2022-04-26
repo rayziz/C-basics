1673f19dc8598605107a083a16fa24168692e22e
rayziz <rayziz@mail.ru>
2022-04-26 13:14:28 +0300

    add Example053

---
+++ Example053_RecReverse/Program.cs
@@ -0,0 +1,15 @@
+﻿/*  Задача 4: Разверните число при помощи рекурсии:
+N = 1234 -> "4321"
+*/
+
+Console.WriteLine(Reverse(1234));
+
+
+// Methods
+
+int Reverse(int value, int lastValue = 0)
+{
+    if (value == 0)
+        return lastValue;
+    return Reverse(value / 10, lastValue * 10 + value % 10);
+}
