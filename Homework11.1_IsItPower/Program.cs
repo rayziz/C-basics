4cd94faaa41f3ced7ebec200637c1dffd9593955
rayziz <rayziz@mail.ru>
2022-04-28 17:53:52 +0300

    add Homework11.1

---
+++ Homework11.1_IsItPower/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 1: Определите, является ли число степенью двойки:
+N = 16 -> "Является степень двойки"
+N = 12 -> “Не является степенью двойки”
+*/
+
+
+Console.WriteLine(VerifyDecimal(1));
+
+
+// Methods
+
+bool VerifyDecimal(int value)
+{
+    if (value <= 0)
+        return false;
+    if (value == 1)
+        return true;    //True т.к. 2 в нулевой степени это 1
+    while ((value % 2) == 0)
+    {
+        if ((value /= 2) == 1)
+            return true;
+    }
+    return false;
+}
+
+
